b32d0ade13e0f2ac75ad7178a9c366132aed587b
User <User@LEWHAN>
2024-11-11 23:15:38 +0530

    Add project files.

---
+++ BasicGUI.Designer.cs
@@ -0,0 +1,307 @@
+﻿using System;
+
+namespace SerialGUISample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serial = new System.IO.Ports.SerialPort(this.components);
+            this.getIOtimer = new System.Windows.Forms.Timer(this.components);
+            this.InputBox1 = new System.Windows.Forms.TextBox();
+            this.Send1 = new System.Windows.Forms.Button();
+            this.Send2 = new System.Windows.Forms.Button();
+            this.statusBox = new System.Windows.Forms.TextBox();
+            this.InputBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.InputBox3 = new System.Windows.Forms.TextBox();
+            this.InputBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.stopWatchTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // serial
+            // 
+            this.serial.PortName = "COM4";
+            // 
+            // getIOtimer
+            // 
+            this.getIOtimer.Enabled = true;
+            this.getIOtimer.Interval = 10;
+            this.getIOtimer.Tick += new System.EventHandler(this.getIOtimer_Tick);
+            // 
+            // InputBox1
+            // 
+            this.InputBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputBox1.Location = new System.Drawing.Point(227, 172);
+            this.InputBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.InputBox1.Name = "InputBox1";
+            this.InputBox1.Size = new System.Drawing.Size(187, 39);
+            this.InputBox1.TabIndex = 0;
+            this.InputBox1.Text = "0";
+            this.InputBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Send1
+            // 
+            this.Send1.BackColor = System.Drawing.Color.LimeGreen;
+            this.Send1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Send1.Location = new System.Drawing.Point(29, 47);
+            this.Send1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Send1.Name = "Send1";
+            this.Send1.Size = new System.Drawing.Size(170, 46);
+            this.Send1.TabIndex = 4;
+            this.Send1.Text = "START";
+            this.Send1.UseVisualStyleBackColor = false;
+            this.Send1.Click += new System.EventHandler(this.Send1_Click);
+            // 
+            // Send2
+            // 
+            this.Send2.BackColor = System.Drawing.Color.Red;
+            this.Send2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Send2.Location = new System.Drawing.Point(29, 134);
+            this.Send2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Send2.Name = "Send2";
+            this.Send2.Size = new System.Drawing.Size(170, 46);
+            this.Send2.TabIndex = 4;
+            this.Send2.Text = "STOP";
+            this.Send2.UseVisualStyleBackColor = false;
+            this.Send2.Click += new System.EventHandler(this.Send2_Click);
+            // 
+            // statusBox
+            // 
+            this.statusBox.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusBox.Location = new System.Drawing.Point(143, 423);
+            this.statusBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(187, 29);
+            this.statusBox.TabIndex = 5;
+            // 
+            // InputBox2
+            // 
+            this.InputBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputBox2.Location = new System.Drawing.Point(466, 172);
+            this.InputBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.InputBox2.Name = "InputBox2";
+            this.InputBox2.Size = new System.Drawing.Size(187, 39);
+            this.InputBox2.TabIndex = 0;
+            this.InputBox2.Text = "0";
+            this.InputBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(395, 423);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 29);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Teal;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label2.Location = new System.Drawing.Point(225, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Left Sensor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label3.Location = new System.Drawing.Point(392, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Select COM Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(501, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 32);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Right Sensor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(225, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 32);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Left Motor Duty %";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(462, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(228, 32);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Right Motor Duty %";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.progressBar1.Location = new System.Drawing.Point(227, 266);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(187, 23);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.progressBar2.Location = new System.Drawing.Point(467, 266);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(187, 23);
+            this.progressBar2.TabIndex = 16;
+            // 
+            // InputBox3
+            // 
+            this.InputBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputBox3.Location = new System.Drawing.Point(227, 316);
+            this.InputBox3.Name = "InputBox3";
+            this.InputBox3.Size = new System.Drawing.Size(187, 39);
+            this.InputBox3.TabIndex = 17;
+            this.InputBox3.Text = "0";
+            this.InputBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // InputBox4
+            // 
+            this.InputBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputBox4.Location = new System.Drawing.Point(467, 316);
+            this.InputBox4.Name = "InputBox4";
+            this.InputBox4.Size = new System.Drawing.Size(187, 39);
+            this.InputBox4.TabIndex = 18;
+            this.InputBox4.Text = "0";
+            this.InputBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 48);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Calibrate Sensors";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(300, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 35);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // stopWatchTimer
+            // 
+            this.stopWatchTimer.Tick += new System.EventHandler(this.stopWatchTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(690, 524);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InputBox4);
+            this.Controls.Add(this.InputBox3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.Send2);
+            this.Controls.Add(this.Send1);
+            this.Controls.Add(this.InputBox2);
+            this.Controls.Add(this.InputBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Desgin Project GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Timer getIOtimer;
+        private System.Windows.Forms.TextBox InputBox1;
+        private System.IO.Ports.SerialPort serial;
+        private System.Windows.Forms.Button Send1;
+        private System.Windows.Forms.Button Send2;
+        private System.Windows.Forms.TextBox statusBox;
+        private System.Windows.Forms.TextBox InputBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TextBox InputBox3;
+        private System.Windows.Forms.TextBox InputBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer stopWatchTimer;
+    }
+}
+
---
+++ BasicGUI.cs
@@ -0,0 +1,223 @@
+﻿//MXEN 3000 line follower GUI
+//Members - Lewhan Balasooriya, Oneli Haloluwa, Adithya Kulasekaram, Lushane de Silva, Laksika Panditharatne
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SerialGUISample
+{    
+
+    public partial class Form1 : Form
+    {
+        // Declare variables to store inputs and outputs.
+        bool runSerial = true;
+        bool byteRead = false;
+        int Input1 = 0;
+        int Input2 = 0;
+        int Input3 = 0;
+        int Input4 = 0;
+
+        string Input1String;
+        string Input2String;
+        string Input3String;
+        string Input4String;
+
+        byte[] Outputs = new byte[4];
+        byte[] Inputs = new byte[6];
+
+        const byte START = 255;
+        const byte ZERO = 0;
+        byte OPERATION = 0;
+        byte CALIBRATE = 0;
+
+        private Stopwatch stopwatch = new Stopwatch();
+
+        public Form1()
+        {
+            // Initialize required for form controls.
+            InitializeComponent();
+
+            // Establish connection with serial
+            if (runSerial == true)
+            {
+                if (!serial.IsOpen)                                  // Check if the serial has been connected.
+                {
+                    try
+                    {
+                        serial.Open();                               //Try to connect to the serial.
+                    }
+                    catch
+                    {
+                        statusBox.Enabled = false;
+                        statusBox.Text ="ERROR: Failed to connect.";     //If the serial does not connect return an error.
+                    }
+                }
+            }
+        }
+
+        private void Send1_Click(object sender, EventArgs e) //Press the button to send the value to start
+        {
+            OPERATION = 1; // The value 2 indicates Output1, value for output set in OutputBox1.
+            sendIO(OPERATION, CALIBRATE);//send data transmission
+            statusBox.Text = "Running";
+            stopwatch.Reset(); // Reset the stopwatch
+            stopwatch.Start(); // Start the stopwatch
+            stopWatchTimer.Start(); // Start the timer
+        }
+
+        private void Send2_Click(object sender, EventArgs e) //Press the button to send the value to stop
+        {
+            OPERATION = 0;
+            sendIO(OPERATION, CALIBRATE); //send data transmission
+            statusBox.Text = "Stopping";
+            stopwatch.Stop(); // Stop the stopwatch
+            stopWatchTimer.Stop(); // Stop the timer
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Press the button to send the value to calibrate sensors
+        {
+            CALIBRATE = 1;
+            sendIO(OPERATION, CALIBRATE); //send data transmission
+            statusBox.Text = "Calibrating";
+        }
+
+        private void sendIO(int STARTSTOP, int CALIBRATION)
+        {
+            Outputs[0] = START;    //Start value that indicates the beginning of the message.
+            Outputs[1] = (byte)STARTSTOP;     //The value for the operation, start/stop
+            Outputs[2] = (byte)CALIBRATION;  //The value for calibration
+            Outputs[3] = (byte)(START + (byte)STARTSTOP + (byte)CALIBRATION); //The checksum byte to confirm the message was received correctly.
+
+            if (serial.IsOpen)
+            {
+                serial.Write(Outputs, 0, 4); //Send all four bytes.                      
+            }
+        }
+
+        
+
+        private void getIOtimer_Tick(object sender, EventArgs e) //It is best to continuously check for incoming data as handling the buffer or waiting for event is not practical in C#.
+        {
+
+            if (serial.IsOpen) //Check that a serial connection exists.
+            {
+                
+                if (serial.BytesToRead >= 6) //Check that the buffer contains a full four byte package.
+                {
+                    statusBox.Text = "Incoming"; 
+                    Inputs[0] = (byte)serial.ReadByte(); //Read the first byte of the package.
+
+                    if (Inputs[0] == START) //Check if the first byte is the start byte.
+                    {
+                        statusBox.Text = "Start Accepted";
+
+                        //Read the data to varaibles
+                        Inputs[1] = (byte)serial.ReadByte();
+                        Inputs[2] = (byte)serial.ReadByte();
+                        Inputs[3] = (byte)serial.ReadByte();
+                        Inputs[4] = (byte)serial.ReadByte();
+                        Inputs[5] = (byte)serial.ReadByte();
+
+                        //Calculate the checksum.
+                        byte checkSum = (byte)(Inputs[0] + Inputs[1] + Inputs[2] + Inputs[3] + Inputs[4]);
+                        statusBox.Text = checkSum.ToString();
+                        //Check that the calculated check sum matches the checksum sent with the message.
+                        if (Inputs[5] == checkSum)
+                        {
+                            statusBox.Text = "Data Recieved";//indicating message recived is correct
+
+                          
+                            Input1 = Inputs[1];// indiacte the postion of the left sensor
+                            if (Input1 == 0)
+                            {
+                                InputBox1.Text = "OUT";
+                            }
+                            else if(Input1 == 1)
+                            {
+                                InputBox1.Text = "IN";
+                            }
+                            else 
+                            {
+                                InputBox1.Text = "ERROR";
+                            }
+
+                            Input2 = Inputs[2];// indiacte the postion of the right sensor
+                            if (Input2 == 0)
+                            {
+                                InputBox2.Text = "OUT";
+                            }
+                            else if(Input2 == 1)
+                            {
+                                InputBox2.Text = "IN";
+                            }
+                            else 
+                            {
+                                InputBox2.Text = "ERROR";
+                            }
+
+                            Input3 = Inputs[3];// display the duty cycle on a progressbar and textbox
+                            
+                            Input3String = Input3.ToString();
+                            InputBox3.Text = Input3String;
+                            progressBar1.Value = Input3;
+
+                            Input4 = Inputs[4];// display the duty cycle on a progressbar and textbox
+
+                            Input4String = Input4.ToString();
+                            InputBox4.Text = Input4String;
+                            progressBar2.Value = Input4;
+                        
+                            CALIBRATE = 0; //Reset calibration variable
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //Obtain com port names
+        {
+            string[] ports= SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) // This allows the user to select the desiered COM port, if multiple COM ports are available
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                serial.PortName = comboBox1.SelectedItem.ToString();  // Set the COM port to the selected value
+                statusBox.Text = "Port set to " + serial.PortName;
+
+                try
+                {
+                    if (!serial.IsOpen) //Check if Serial port is not already open
+                    {
+                        serial.Open();  // Try to open the serial connection
+                        statusBox.Text = "Connected to " + serial.PortName; // If Serial connection is established shows the COM port name on the statusbox
+                    }
+                }
+                catch 
+                {
+                    statusBox.Enabled = false;
+                    statusBox.Text = "ERROR: Failed to connect."; //display the connection error
+                }
+            }
+        }
+
+        private void stopWatchTimer_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = stopwatch.Elapsed.ToString(@"hh\:mm\:ss\.fff"); //Display the stop watch time on text box
+        }
+
+        
+
+    }
+    
+}
